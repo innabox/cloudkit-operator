6729bd67f323239750356ed79f70460880f04d45
Adrien Gentil <adrien.gentil@free.fr>
2025-12-11 13:42:55 +0100

    Tidy-up VirtualMachineReference (#89)
    This change updates the namespace in VirtualMachineReference, and
    cleans-up roleBindingName and serviceAccountName as it is not used.

--- api/v1alpha1/virtualmachine_virtualmachinereference.go
+++ api/v1alpha1/virtualmachine_virtualmachinereference.go
@@ -10,16 +10,6 @@ func (vm *VirtualMachine) SetVirtualMachineReferenceKubeVirtVirtalMachineName(na
 	vm.Status.VirtualMachineReference.KubeVirtVirtalMachineName = name
 }
 
-func (vm *VirtualMachine) SetVirtualMachineReferenceServiceAccountName(name string) {
-	vm.EnsureVirtualMachineReference()
-	vm.Status.VirtualMachineReference.ServiceAccountName = name
-}
-
-func (vm *VirtualMachine) SetVirtualMachineReferenceRoleBindingName(name string) {
-	vm.EnsureVirtualMachineReference()
-	vm.Status.VirtualMachineReference.RoleBindingName = name
-}
-
 func (vm *VirtualMachine) EnsureVirtualMachineReference() {
 	if vm.Status.VirtualMachineReference == nil {
 		vm.Status.VirtualMachineReference = &VirtualMachineReferenceType{}
@@ -38,18 +28,4 @@ func (vm *VirtualMachine) GetVirtualMachineReferenceKubeVirtVirtalMachineName() 
 		return ""
 	}
 	return vm.Status.VirtualMachineReference.KubeVirtVirtalMachineName
-}
-
-func (vm *VirtualMachine) GetVirtualMachineReferenceServiceAccountName() string {
-	if vm.Status.VirtualMachineReference == nil {
-		return ""
-	}
-	return vm.Status.VirtualMachineReference.ServiceAccountName
-}
-
-func (vm *VirtualMachine) GetVirtualMachineReferenceRoleBindingName() string {
-	if vm.Status.VirtualMachineReference == nil {
-		return ""
-	}
-	return vm.Status.VirtualMachineReference.RoleBindingName
 }
